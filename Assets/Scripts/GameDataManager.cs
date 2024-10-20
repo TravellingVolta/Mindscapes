dcfd8a7f22315a4c69ce6ab8a9a0d3e2fe7dbccb
kaiyang <yangkai92@gmail.com>
2024-10-20 17:11:54 -0600

    Add GameDataManager; Add FlippingCard; Modify Game Scene

---
+++ Assets/Scripts/GameDataManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using GameDataEditor;
+
+public class GameDataManager : Singleton<GameDataManager>
+{
+    private const string currentTable = "gde_data";
+
+    private Dictionary<string, GDEFlashCardsData> flashCardsTableData = new();
+    public GDEFlashCardsData GetFlashCardsDat(string key)
+    {
+        if (key != null && flashCardsTableData.ContainsKey(key))
+        {
+            return flashCardsTableData[key];
+        }
+        else
+        {
+            Debug.LogWarning("Empty key: " + key);
+
+            return null;
+        }
+    }
+
+    private void Awake()
+    {
+        InitGDEData();
+    }
+
+    private void OnDestroy()
+    {
+        flashCardsTableData.Clear();
+    }
+
+    public void InitGDEData()
+    {
+        if (!GDEDataManager.Init(currentTable))
+        {
+            return;
+        }
+
+        foreach (var one in GDEDataManager.GetAllItems<GDEFlashCardsData>())
+        {
+            flashCardsTableData.Add(one.Key, one);
+        }
+
+    }
+
+    public void ClearGameData()
+    {
+
+    }
+
+}
